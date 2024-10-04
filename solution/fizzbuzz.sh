1d6b6f236792825cc846dc493634cb6c0e49615f
Evanderer <evelinavinnikova@gmail.com>
2024-10-05 00:33:55 +0700

    Upd

--- solution/fizzbuzz.sh
+++ solution/fizzbuzz.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-for ((i=1; i<="$1"; i++)); do
-
-  if ((i%3 == 0 && i%5 == 0)); then
-    echo "Fizz Buzz"
-
-  elif (( i%5 == 0 )); then
-    echo "Buzz"
-
-  elif (( i%3 == 0 )); then
-    echo "Fizz"
-
-  else
-    echo "$i"
-  fi
+for (( i=1; i<=$1; i++ )); do
+	if (( i % 3 == 0 && i % 5 == 0 )); then
+		echo "Fizz Buzz"
+	elif (( i % 3 == 0 )); then
+		echo "Fizz"
+	elif (( i % 5 == 0 )); then
+		echo "Buzz"
+	else
+		echo "$i"
+	fi
 done
