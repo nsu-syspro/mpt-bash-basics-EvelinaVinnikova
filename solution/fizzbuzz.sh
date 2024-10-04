06551e21022913a54d01d6c68da0803bb1098a5d
Evanderer <evelinavinnikova@gmail.com>
2024-10-05 00:04:29 +0700

    Upd

--- solution/fizzbuzz.sh
+++ solution/fizzbuzz.sh
@@ -4,18 +4,20 @@ n="$1"
 
 for ((i=1; i<=n; i++)); do
     output=""
-    
+
     if (( i % 3 == 0 )); then
         output+="Fizz"
     fi
-    
+
     if (( i % 5 == 0 )); then
-        output+=" Buzz"
+        output+="Buzz"
     fi
-    
+
     if [[ -z "$output" ]]; then
         output="$i"
+    else
+        output+=" "
     fi
-    
+
     printf "%s\n" "$output"
 done
